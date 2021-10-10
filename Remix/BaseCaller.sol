0796124bd3055271e009eb245c4648ce70f18ca7
sǝunN oısıɔɹɐ⊥ <tarcnux@gmail.com>
2021-10-10 18:03:23 -0300

    Create BaseCaller.sol
    /**
    * https://gist.github.com/ConsenSys-Academy/be7dacf4d313ea4c1347638e921541b5
    * Exemplo de interação entre dois contratos
    */

---
+++ Remix/BaseCaller.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Base {
+    
+    uint x;
+    
+    constructor() public {
+        x = 10;
+    }
+    
+    function setX(uint _x) public returns(bool) {
+        x = _x;
+        return true;
+    }
+    
+    function getX() public view returns(uint) {
+        return x;
+    }
+}
+
+contract Caller {
+    
+    function getBaseX(address baseAddress) public view returns(uint) {
+        Base baseContract = Base(baseAddress);
+        return baseContract.getX();
+    }
+    
+}
